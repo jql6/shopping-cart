fa516fb3d846a86d8807ab937c5c7d23e74cc3aa
jql6 <jql6@sfu.ca>
2021-07-17 15:08:55 -0700

    Add cart page to app

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import App from "./App";
 import Menu from "./Menu";
+import Cart from "./Cart";
 
 const Routes = () => {
   return (
@@ -12,13 +13,17 @@ const Routes = () => {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/shop">Shop</Link>
+            <Link to="/menu">Menu</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
           </li>
         </ul>
       </nav>
       <Switch>
         <Route exact path="/" component={App} />
-        <Route exact path="/shop" component={Menu} />
+        <Route exact path="/menu" component={Menu} />
+        <Route exact path="/cart" component={Cart} />
       </Switch>
     </BrowserRouter>
   );
