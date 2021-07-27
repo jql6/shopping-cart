ca10e35db7109698d5cd8f1b1312d16999c4cbfc
jql6 <jql6@sfu.ca>
2021-07-27 14:50:29 -0700

    Add cart component

--- src/CartPage.js
+++ src/CartPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./CartPage.css";
+import Cart from "./components/Cart";
 
 function CartPage() {
   return (
     <div className="Cart">
       <h1 className="page-header">Cart</h1>
+      <Cart />
     </div>
   );
 }
---
+++ src/components/Cart.js
@@ -0,0 +1,37 @@
+// Cart.js
+import "./Cart.css";
+import React, { useState } from "react";
+import fullMenu from "./../Menu";
+
+function Cart() {
+  // Create a card list
+  const [menuItems] = useState(fullMenu.food);
+
+  const [drinkItems] = useState(fullMenu.drinks);
+
+  return (
+    <div>
+      <h2>Food</h2>
+      {menuItems.map((item) => {
+        return (
+          <div key={item.name}>
+            <div>{item.name}</div>
+            <div>{item.quantity}</div>
+          </div>
+        );
+      })}
+      <h2>Drinks</h2>
+      {drinkItems.map((item) => {
+        return (
+          <div key={item.name}>
+            <div>{item.name}</div>
+            <div>{item.quantity}</div>
+          </div>
+        );
+      })}
+      <button>Checkout</button>
+    </div>
+  );
+}
+
+export default Cart;
