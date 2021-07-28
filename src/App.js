9200c3c4d3b0f6a3a7504317d944dc2c7f9bb46f
jql6 <jql6@sfu.ca>
2021-07-28 16:24:13 -0700

    Add name and description to homepage

--- src/App.js
+++ src/App.js
@@ -17,15 +17,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="page-header">Homepage</h1>
+      <h1 className="page-header">Quesadilla Bravas</h1>
       <div id="homepage-image">
         <div className="homepage-image-text">
+          <p className={"italics"}>
+            The health and safety of our staff and community is our number one
+            priority. Our restaurant is committed to practicing the strictest
+            Covid-19 protocols set by health and government officials.
+          </p>
           <p>
-            Example description Example description Example description Example
-            description Example description Example description Example
-            description Example description Example description Example
-            description Example description Example description Example
-            description Example description
+            As a leader in the Mexican food industry, Quesadilla Bravas is proud
+            of its reputation for excellent food. Each dish is jam-packed with
+            cheese and no tortilla is ever left too long on the stove.
           </p>
         </div>
       </div>
