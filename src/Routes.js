bad5a91670aad23db7831c48f6bf5dcccbdb3814
jql6 <jql6@sfu.ca>
2021-07-29 10:26:52 -0700

    Make links subpages

--- src/Routes.js
+++ src/Routes.js
@@ -25,21 +25,25 @@ const Routes = () => {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/shopping-cart">Home</Link>
           </li>
           <li>
-            <Link to="/menu">Menu</Link>
+            <Link to="/shopping-cart/menu">Menu</Link>
           </li>
           <li>
-            <Link to="/cart">Cart: {cartTotal}</Link>
+            <Link to="/shopping-cart/cart">Cart: {cartTotal}</Link>
           </li>
         </ul>
       </nav>
       <Switch>
-        <Route exact path="/" render={(props) => <App {...props} />} />
         <Route
           exact
-          path="/menu"
+          path="/shopping-cart"
+          render={(props) => <App {...props} />}
+        />
+        <Route
+          exact
+          path="/shopping-cart/menu"
           render={(props) => (
             <MenuPage
               setCartItems={setCartItems}
@@ -52,7 +56,7 @@ const Routes = () => {
         />
         <Route
           exact
-          path="/cart"
+          path="/shopping-cart/cart"
           render={(props) => <CartPage cartItems={cartItems} {...props} />}
         />
       </Switch>
