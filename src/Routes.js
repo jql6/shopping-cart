5e65de9c1db53f67dce0c807c4cb264bc6a47f2b
jql6 <jql6@sfu.ca>
2021-07-28 22:15:45 -0700

    Add links to repo and homepage, as well as meta info

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,7 @@
+// Routes.js
+import "./Routes.css";
 import React, { useState } from "react";
+import { Helmet } from "react-helmet";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import App from "./App";
 import MenuPage from "./MenuPage";
@@ -11,6 +14,12 @@ const Routes = () => {
 
   return (
     <BrowserRouter>
+      <Helmet>
+        <title>Quesadilla Bravas</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charset="UTF-8" />
+        <meta name="description" content="Mexican restaurant website" />
+      </Helmet>
       <nav>
         <ul>
           <li>
